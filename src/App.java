acf05e8d82845ccc2ce525c7f8ca607f8bf83aae
Juan Vacca <clanbs11.12@gmail.com>
2025-07-14 15:46:39 -0500

    instacia de clase Coche

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var carro = new Coche("Renault", "Duster", 2015);
+
+        System.out.println(carro.MostrarEstado());
     }
 }
