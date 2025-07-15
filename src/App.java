bc4a0ec9426331396fa865d4c43b9866da4d867e
Juan Vacca <clanbs11.12@gmail.com>
2025-07-15 07:42:33 -0500

    Ejecucion de métodos

--- src/App.java
+++ src/App.java
@@ -3,5 +3,24 @@ public class App {
         var carro = new Coche("Renault", "Duster", 2015);
 
         System.out.println(carro.MostrarEstado());
+
+        carro.TurnOnEngine();
+
+        System.out.println(carro.MostrarEstado());
+
+        carro.Accelerate(10.0);
+
+        System.out.println(carro.MostrarEstado());
+
+        
+
+        carro.Stop(11.0);
+
+        carro.Stop(10.0);
+
+        carro.TurnOffEngine();
+
+        carro.Stop(10.0);
+
     }
 }
